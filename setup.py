d14c4224a4dd78382af648089d6068b1bf4e7805
Pierre Ernst <pierre-ernst@users.noreply.github.com>
2020-07-15 14:50:47 -0400

    Bump Python version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     long_description=long_desc,
     install_requires=install_requires,
     extras_require={'graphviz': ['graphviz']},
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    python_requires='>=3.7',
     py_modules=['pipdeptree'],
     entry_points={
         'console_scripts': [
@@ -40,12 +40,6 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ]
